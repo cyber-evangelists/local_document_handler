9689f5ee4f3ffaef32837f99587560b51ea923bd
Talha Zafar <49996963+talhazafarjutt@users.noreply.github.com>
2023-08-28 18:28:55 +0500

    scan
    added file scanning functionality while upload and download

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify, send_file
 
 from flask_mysqldb import MySQL
 from nextcloud import NextCloud
+from virus_total_apis import PublicApi
+import hashlib
 import os
 
 app = Flask(__name__)
@@ -15,21 +17,27 @@ app.config["MYSQL_DB"] = "files_data"
 
 mysql = MySQL(app)
 
+def scanner(filename):
+    API_KEY = '88858f77a2c07a9d4e9b6b730ea378d1d2c02e3c9e3e9ff885cb1e50da23014b'
+    with open(filename, "rb") as file:
+        EICAR_MD5 = hashlib.md5(file.read()).hexdigest()
 
-@app.route("/")
-def hello_world():
-    return "Hello, Docker Compose Flask and MySQL!"
+    vt = PublicApi(API_KEY)
+    response = vt.get_file_report(EICAR_MD5)
+   
+    if response["response_code"] == 200:
+        return response["results"]["response_code"] == 0 or response["results"]["positives"] == 0
+    return False
 
 
 @app.route("/get_data")
 def fetch_data():
     try:
-        # conn = my_database.connect()
-        # conn.close()
         cursor = mysql.connection.cursor()
         return "Database connection successful!"
     except Exception as e:
         return f"Database connection failed: {str(e)}"
+
 
 @app.route('/getfiles',methods=['POST'])
 def get_files_name():
@@ -50,8 +58,10 @@ def get_files_name():
     _list_rec(root)
     return file_name
 
+
 @app.route('/upload_file',methods=['POST'])
 def upload_file():
+    check = None
     username = request.form.get('username')
     password = request.form.get('password')
     if 'file' not in request.files:
@@ -59,13 +69,17 @@ def upload_file():
     nxc = NextCloud(endpoint='http://host.docker.internal:8080/', user=username, password=password, json_output=True)
     file = request.files['file']
     file.save(file.filename)
-    check = nxc.upload_file(file.filename, '/local/'+file.filename).data
-    if check=='':
+    if scanner(file.filename):
+        check = nxc.upload_file(file.filename, '/local/'+file.filename).data
         os.remove(file.filename)
-        return 'file uploaded successfully'
+        if check=='':
+            return 'file uploaded successfully'
+        else:
+            return 'file upload failed'
     else:
         os.remove(file.filename)
-        return 'error while uploading file' 
+        return 'error while uploading file or file has virus' 
+
 
 @app.route('/get_file',methods=['GET'])
 def get_file():
@@ -76,9 +90,13 @@ def get_file():
     file = nxc.get_file('/local/'+filename)
     file.fetch_file_content()
     file.download()
-    response = send_file(filename, as_attachment=True)
-    os.remove(filename)
-    return response
+    if scanner(filename):
+        response = send_file(filename, as_attachment=True)
+        os.remove(filename)
+        return response
+    else:
+        os.remove(filename)
+        return 'file has virus'
 
 
 if __name__ == "__main__":
