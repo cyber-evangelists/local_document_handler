c0a112eceb99fc58626c45daf3fda6ad75941d0d
Talha Zafar <49996963+talhazafarjutt@users.noreply.github.com>
2023-09-22 15:26:04 +0500

    app log

--- db/init.sql
+++ db/init.sql
@@ -1,9 +1,6 @@
--- CREATE DATABASE file_data;
 
--- Switch to the created database
 USE file_data;
 
--- Create the 'files' table
 CREATE TABLE files (
     id INT AUTO_INCREMENT PRIMARY KEY,
     file_name VARCHAR(255),
