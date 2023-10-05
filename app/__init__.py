93d4381b27d2c577959eaf75fb1be40896f907a4
Talha Zafar <talha.zafar.j@gmail.com>
2023-10-05 20:24:39 +0500

    sqlalchemy added

--- app/__init__.py
+++ app/__init__.py
@@ -6,15 +6,19 @@ This is a Flask API that provides various endpoints for accessing and manipulati
 """
 
 from flask_mysqldb import MySQL
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from services.middleware import middleware
 from services.config import Config
+from sqlalchemy_utils.functions import database_exists, create_database
 
 app = Flask(__name__)
 
 app.wsgi_app = middleware(app.wsgi_app)
 app.config.from_object(Config)
 mysql = MySQL(app)
+db = SQLAlchemy(app)
+
 # app.config['CORS_HEADERS'] = 'Content-Type'
 
 # @app.after_request
@@ -23,6 +27,11 @@ mysql = MySQL(app)
 #     response.headers.add('Access-Control-Allow-Origin', '*')
 #     response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
 #     return response
+with app.app_context():
+    if not database_exists('mysql+pymysql://root:example@localhost:3306/document_handler'):
+        create_database('mysql+pymysql://root:example@localhost:3306/document_handler')
+    db.create_all()
+
 
 CORS(app, resources={r"*": {"origins": "*"}})
 
